9524ad0d1f1b7b2afce34701a9ad41d8f589285f
Michelle Wingter <mwingter@gmail.com>
2019-04-03 12:28:50 -0300

    No meaningful changes here

--- t1.c
+++ t1.c
@@ -1,61 +1,50 @@
 /*
- * Michelle Wingter da Silva
- * 10783243
+ *	Trabalho 1
+ *	SCC0215 - Organização de Arquivos
+ * 	Este trabalho tem como objetivo armazenar dados em um arquivo binário de
+ * acordo com uma organização de campos e registros, bem como recuperar os
+ * dados armazenados.
+ * Nome: Michelle Wingter da Silva
+ * nUSP: 10783243
  */
 
-/*
-fopen, fread, fwrite, fseek, fprintf, fscanf, fclose
+	//Struct do Registro de dados
+	typedef struct regdados_ REGDADOS;
+	struct regdados_{
+		int idServidor;
+		double salarioServidor;
+		char telefoneServidor[14];
+		int tamNomeServidor;
+		char *nomeServidor;
+		int tamCargoSuperior;
+		char *cargoSuperior;
+	};
 
-Funções úteis:
-	fread(buffer, n, 1, file)
-	fwrite(buffer, n, 1, file)
-	fscanf(file, "%14s", str) //Ler data:
-	fscanf(file, "%d", n) //Ler indicadores de tamanho
-	fseek(file, x, SEEK_CUR | SEEK_SET | SEEK_END) //pular x bytes
 
-	Exemplo de Makefile:
-	all:
-		gcc *.c -o programaTrab1
-	run:
-		./programaTrab1
-*/
+	//Struct do Registro de Cabeçalho
+	typedef struct redcab_ REGCAB;
+	struct redcab_{
+		char status;
+		int topoLista; //sempre igual à -1
+		char tags[5];
+		char campos[5][40];
+	};
+
+	//Struct do Buffer-Pool
+	typedef struct buffer_ BUFFER;
+	struct buffer_{
+		int nPaginas;
+		char paginas[x][32000]; //x a definir
+	};
+
+
+
 
 int main(){
 
 
-	//Struct do Registro de dados
-	 typedef struct regdados_ REGDADOS;
-	 struct regdados_{
-	 	int idServidor;
-	 	double salarioServidor;
-	 	char telefoneServidor[14];
-	 	int tamNomeServidor;
-	 	char *nomeServidor;
-	 	int tamCargoSuperior;
-	 	char *cargoSuperior;
-	 };
-
-
-	 //Struct do Registro de Cabeçalho
-	 typedef struct redcab_ REGCAB;
-	 struct redcab_{
-	 	char status;
-	 	int topoLista; //sempre igual à -1
-	 	char tags[5];
-	 	char campos[5][40];
-	 };
-
-	 //Struct do Buffer-Pool
-	 typedef struct buffer_ BUFFER;
-	 struct buffer_{
-	 	int nPaginas;
-	 	char paginas[x][32000]; //x a definir
-	 };
 
 
 
-
-
-
-	 return 0;
+	return 0;
 }
