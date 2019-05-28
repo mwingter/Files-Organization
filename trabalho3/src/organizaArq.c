d33335e911b7171e1300ffff9126030535a4327b
Michelle <mwingter@gmail.com>
2019-05-28 19:54:43 -0300

    funcionalidade 7 completa!
    casos 1 a 4, 8 a 10, 14 a 16 ok!

--- trabalho3/src/organizaArq.c
+++ trabalho3/src/organizaArq.c
@@ -47,6 +47,8 @@ void ordena_por_id(char *nomeBin_in, char *nomeBin_out){
 	int n_reg = 0; //numero de registros na lista
 	int tam_pagina = 0; //a função q le um registro do arquivo e salva na struct precisa desse parâmetro, mas aqui não será necessário
 	int tam_ult_registro = 0;
+	char removido = '@';
+	int tam_reg_aux = 0;
 
 	//percorrendo o arquivo e montando a lista de structs de registros de dados
 	//printf("ANTES DE ORDENAR\n");
@@ -55,14 +57,45 @@ void ordena_por_id(char *nomeBin_in, char *nomeBin_out){
 		lista = realloc(lista, n_reg * sizeof(REGDADOS));
 		lista[n_reg - 1].nomeServidor[0] = '\0';
 		lista[n_reg - 1].cargoServidor[0] = '\0';
-		leUmRegistroBin(bin_in, &lista[n_reg - 1], &tam_pagina);
-		//printaUmReg(&lista[n_reg - 1]);
-
-		if(tam_pagina >= TAM_PAG_DISCO){ //se for o ultimo registro da pagina
+		lista[n_reg - 1].idServidor = -1;
+		fread(&removido,REM_TAM,1,bin_in);
+		//printf("removido = %c\n", removido);
+		fread(&tam_reg_aux,TAM_TAM,1,bin_in);
+		fseek(bin_in, - (REM_TAM + TAM_TAM), SEEK_CUR);
+		//if(removido == '-'){
+			leUmRegistroBin(bin_in, &lista[n_reg - 1], &tam_pagina);
+			//printaUmReg(&lista[n_reg - 1]);
 			tam_ult_registro = recalcula_tam_registro(&lista[n_reg - 1]);
+			//printf("\tantes tam = %d\n\t depois tam_ult_registro = %d\n", lista[n_reg - 1].tamanhoRegistro, tam_ult_registro);
 			lista[n_reg - 1].tamanhoRegistro = tam_ult_registro;
-			tam_pagina = 0;
-		}
+			if(lista[n_reg - 1].idServidor == -1){
+				//lista[n_reg - 1] = NULL;
+				n_reg--;
+			}
+
+			if(tam_pagina >= TAM_PAG_DISCO){ //se for o ultimo registro da pagina
+				//printf("tam pagina (%d) >= TAM_PAG_DISCO\n", tam_pagina);
+				tam_pagina = tam_ult_registro;
+			}
+		//}
+		/*else{
+			if(removido == '*'){
+				fseek(bin_in, tam_reg_aux, SEEK_CUR);
+				tam_pagina += tam_reg_aux;
+				if(tam_pagina >= TAM_PAG_DISCO){
+					tam_pagina = 0;
+				}			
+			}
+			else{
+				while(!feof(bin_in) && fgetc(bin_in) != '-'){
+					continue;
+				}
+				if (!feof(bin_in)) {
+					fseek(bin_in, -1, SEEK_CUR);
+				}
+			}
+		}*/
+
 	}
 
 	//ordenando pelo id a lista de structs de registros
