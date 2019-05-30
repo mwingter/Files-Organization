03878133619ebc4d9ed05259c6a1d8265a00781d
Michelle <mwingter@gmail.com>
2019-05-29 22:29:49 -0300

    T4 iniciado!!

---
+++ trabalho4/include/organizaArq.h
@@ -0,0 +1,32 @@
+/*
+	TRABALHO 3 - ORGANIZAÇÃO DE ARQUIVOS
+
+	Nome:	Michelle Wingter da Silva	nUSP:	10783243
+			Juliano Fantozzi					9791218
+
+*/
+
+#ifndef ORGANIZAARQ_H
+	#define ORGANIZAARQ_H
+
+	#include "rdados.h"
+	#include "escrevenatela.h"
+	#include "manipulaReg.h"
+	#include "merge_sort.h"
+
+//============================= ORDENAÇÃO INTERNA ===========================
+	void ordena_por_id(char* nomeBin_in, char* nomeBin_out);
+	int int_compare_id(const void *A, const void *B);
+	int recalcula_tam_registro(REGDADOS *rd);
+
+//========================== MERGING DE DOIS ARQUIVOS ======================
+	void read_andMerge(char *nomeBin_in1, char *nomeBin_in2, char *nomeBin_out);
+
+//========================== MATCHING DE DOIS ARQUIVOS ======================
+	void read_andMatch(char *nomeBin_in1, char *nomeBin_in2, char *nomeBin_out);
+
+//========================== 
+	void avancaProximo(REGDADOS *rd1, REGDADOS *rd2, FILE *bin_in1, FILE *bin_in2, int *tam_pagina1, int *tam_pagina2);
+
+
+#endif
